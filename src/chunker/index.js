c5ac34541018464497d9ee4b245a72608fec6aea
Dan Ordille <dordille@gmail.com>
2018-07-25 00:59:45 -0400

    Add support for rabin fingerprinting chunk algorithm

---
+++ src/chunker/index.js
@@ -0,0 +1,12 @@
+const rabin = require('rabin')
+
+const chunkers = {
+  fixed: require('../chunker/fixed-size')
+}
+
+// Don't include the rabin chunker when rabin require is null for browser
+if (rabin) {
+  chunkers.rabin = require('../chunker/rabin')
+}
+
+module.exports = chunkers
