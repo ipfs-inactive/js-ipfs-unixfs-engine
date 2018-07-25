a16863831e21a9bc04d739273558f290032adf93
Dan Ordille <dordille@gmail.com>
2018-07-25 13:01:31 -0400

    Exclude rabin chunker from browser build

--- src/chunker/index.js
+++ src/chunker/index.js
@@ -3,12 +3,8 @@
 const rabin = require('rabin')
 
 const chunkers = {
-  fixed: require('../chunker/fixed-size')
-}
-
-// Don't include the rabin chunker when rabin require is null for browser
-if (rabin) {
-  chunkers.rabin = require('../chunker/rabin')
+  fixed: require('../chunker/fixed-size'),
+  rabin: require('../chunker/rabin')
 }
 
 module.exports = chunkers
